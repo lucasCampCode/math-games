826c376a61772d2f9e9e6dde413628263fd762f7
lucas <lcampbell0815@gmail.com>
2020-10-12 12:28:57 -0500

    added a way to add and remove entity to and from an array

--- MathForGames/Scene.cs
+++ MathForGames/Scene.cs
@@ -15,33 +15,122 @@ namespace MathForGames
 
         public void AddEntity(Entity entity)
         {
+            //creating a new array that has an aditional slot from an old array
             Entity[] newEntities = new Entity[_entities.Length + 1];
+            //copy values from old array to the old array
             for (int i = 0; i < _entities.Length; i++)
             {
                 newEntities[i] = _entities[i];
             }
+            //sets the new entity at the end of the new array
             newEntities[_entities.Length] = entity;
+            //set old array to the new array with the new entity
             _entities = newEntities;
         }
         
-        public void Start()
+        public bool RemoveEntity(int index)
         {
+            //checks to see if index is out of bounds of array
+            if(index < 0 || index >= _entities.Length)
+            {
+                return false;
+            }
+            bool entityRemoved = false;
 
+            //creating a new array that has a slot taken away from an old array
+            Entity[] tempEntity = new Entity[_entities.Length - 1];
+            //create variable to access tempArray index
+            int j = 0;
+            //copy values from the old array to new Array
+            for (int i = 0; i < _entities.Length; i++)
+            {
+                //If the current index is not the index that needs to be removed
+                //add the value into the old array and increment j
+                if (i != index)
+                {
+                    tempEntity[j] = _entities[i];
+                    j++;
+                }
+                else //gives us our return value for debuging
+                {
+                    entityRemoved = true;
+                }
+            }
+            //set the old arrat to be the tempArray
+            _entities = tempEntity;
+
+            return entityRemoved;
         }
 
-        public void Update()
+        public bool RemoveEntity(Entity entity)
         {
+            //if no entity was pass through dont run funtion
+            if (entity == null)
+            {
+                return false;
+            }
 
+            bool entityRemoved = false;
+
+            //creating a new array that has a slot taken away from an old array
+            Entity[] tempEntity = new Entity[_entities.Length - 1];
+            //create variable to access tempArray index
+            int j = 0;
+            //copy values from the old array to new Array
+            for (int i = 0; i < _entities.Length; i++)
+            {
+                //If the current index is not the index that needs to be removed
+                //add the value into the old array and increment j
+                if (entity != _entities[i])
+                {
+                    tempEntity[j] = _entities[i];
+                    j++;
+                }
+                else //gives us our return value for debuging
+                {
+                    entityRemoved = true;
+                }
+            }
+            //set the old arrat to be the tempArray
+            _entities = tempEntity;
+
+            return entityRemoved;
         }
 
-        public void Draw()
+        public virtual void Start()
         {
+            //for each entity
+            for (int i = 0; i < _entities.Length; i++)
+            {
+                _entities[i].Start();
+            }
+        }//start
 
-        }
-
-        public void End()
+        public virtual void Update()
         {
+            //for each entity
+            for (int i = 0; i < _entities.Length; i++)
+            {
+                _entities[i].Update();
+            }
+        }//update
 
-        }
+        public virtual void Draw()
+        {
+            //for each entity
+            for (int i = 0; i < _entities.Length; i++)
+            {
+                _entities[i].Draw();
+            }
+        }//Draw
+
+        public virtual void End()
+        {
+            //for each entity
+            for (int i = 0; i < _entities.Length; i++)
+            {
+                _entities[i].End();
+            }
+        }//End
     }
 }
