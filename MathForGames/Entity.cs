ea193cbb72ee11868e92567907e8474486de4eaa
lucas <lcampbell0815@gmail.com>
2020-10-12 17:04:30 -0500

    added movement keys for a entity to move across the console window

--- MathForGames/Entity.cs
+++ MathForGames/Entity.cs
@@ -1,30 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Numerics;
 using System.Text;
 
 namespace MathForGames
 {
     class Entity
     {
-
+        private char _icon = '-';
+        private int _x = 0;
+        private int _y = 0;
         public void Start()
         {
 
-        }
+        }//start of entity
 
         public void Update()
         {
-
-        }
+            if (Game.CheckKey(ConsoleKey.A)) 
+                _x--;
+            if (Game.CheckKey(ConsoleKey.D))
+                _x++;
+        }//updates of entity
 
         public void Draw()
         {
-
-        }
+            Console.SetCursorPosition(_x,_y);
+            Console.Write(_icon);
+        }//draws the entity
 
         public void End()
-        {
+        {      
 
-        }
+        }//ends the enitities
     }
 }
--- MathForGames/Game.cs
+++ MathForGames/Game.cs
@@ -1,38 +1,55 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Numerics;
 using System.Text;
+using System.Threading;
 
 namespace MathForGames
 {
     class Game
     {
         private static bool _gameOver = false;
-
+        private Scene _scene;
         //Static function used to set game over without an instance of game.
         public static void SetGameOver(bool value)
         {
             _gameOver = value;
         }
 
+        //return weather or not the specified consoleKey is pressed
+        public static bool CheckKey(ConsoleKey key)
+        {
+            if (Console.KeyAvailable)
+            {
+                if (Console.ReadKey(true).Key == key)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
 
         //Called when the game begins. Use this for initialization.
         public void Start()
         {
-
+            _scene = new Scene();
+            Entity entity = new Entity();
+            _scene.AddEntity(entity);
         }
-
 
         //Called every frame.
         public void Update()
         {
-            
+            _scene.Update();
         }
 
         //Used to display objects and other info on the screen.
         public void Draw()
         {
-
+            Console.Clear();
+            _scene.Draw();
         }
 
 
@@ -52,6 +69,9 @@ namespace MathForGames
             {
                 Update();
                 Draw();
+                while (Console.KeyAvailable) 
+                    Console.ReadKey(true);
+                Thread.Sleep(60);
             }
 
             End();
